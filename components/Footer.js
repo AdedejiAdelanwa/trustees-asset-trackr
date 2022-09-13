c89036ed23f316b69a2d2a51ee9c4fdce86026f5
Adedeji Adelanwa <adedejiadelanwa@gmail.com>
2022-09-13 13:57:55 +0100

    fix: REduce footer height

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full bg-[#F3F3F3] text-black text-[1.4rem] h-[10vh] sm:h-[6vh] sm:px-[1.5rem] px-[15.2rem] items-center ">
+    <footer className="flex w-full bg-[#F3F3F3] text-black text-[1.4rem] h-[6vh] sm:px-[1.5rem] px-[15.2rem] items-center ">
       © 2022 Copyright: Meristem
     </footer>
   );
