172c9eb4e6f2b17de5c4c3d01e03c4ddc77ed438
Adedeji Adelanwa <adedejiadelanwa@gmail.com>
2022-09-13 15:40:08 +0100

    feat: Add estate plan page

--- pages/dashboard/estate-plans.js
+++ pages/dashboard/estate-plans.js
@@ -1,24 +1,112 @@
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Flex,
+  Stack,
+  Heading,
+  Text,
+  VStack,
+  Box,
+} from "@chakra-ui/react";
 import DashBoardContainer from "../../components/DashboardLayout";
 import MainHeader from "../../components/MainHeader";
 import SideNav from "../../components/SideNavigation";
-import {BiSearchAlt2} from "react-icons/bi"
+import { BiSearchAlt2 } from "react-icons/bi";
+import { RiFileList3Line } from "react-icons/ri";
+import EstatePlanItem from "../../components/EstatePlanItem";
+import { BsPersonCircle } from "react-icons/bs";
+import SimpleWillCard from "../../components/SimpleWillCard";
 
-export default function EstatePlans(){
-    return(
-        <section className="main-content text-black bg-white w-[80%] h-[90vh] p-[3rem] right-0 absolute overflow-auto">
-       <div className="flex items-center justify-between">
-       <h2 className="text-[2.8rem] font-bold">My Estate Plans </h2>
+const estateplanList = [
+  { name: "simple will", status: "processing" },
+  { name: "comprehensive will", status: "active" },
+  { name: "mkat", status: "processing" },
+  { name: "mfat", status: "active" },
+];
+const beneficiaries = [
+  { name: "Peterson Omoboriowo", relationship: "son" },
+  { name: "Paula Omoboriowo", relationship: "daughter" },
+];
+export default function EstatePlans() {
+  return (
+    <section className="main-content text-black bg-white w-[80%] sm:w-[100%!important] md:w-[88%]   h-[90vh] p-[3rem] sm:px-[1.5rem] right-0 absolute overflow-auto">
+      <div className="flex items-center justify-between">
+        <h2 className="text-[2.8rem] font-bold">My Estate Plans </h2>
         <BiSearchAlt2 fontSize={"2rem"} className="cursor-pointer" />
-       </div>
-        </section>
-    )
+      </div>
+      <Tabs mt={"3rem"} fontSize="1.6rem">
+        <TabList borderBottomColor={"grey"}>
+          <Tab fontSize="1.6rem">My Estate Plans</Tab>
+          <Tab fontSize="1.6rem">Beneficiaries</Tab>
+          <Tab fontSize="1.6rem">Add Estate Plan</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Flex
+              flexWrap="wrap"
+              justifyContent={"space-between"}
+              gap="2rem"
+              mt={"5rem"}
+            >
+              {estateplanList.map((item, i) => (
+                <EstatePlanItem key={i}>
+                  <RiFileList3Line fontSize={"2.5rem"} color="darkgreen" />
+
+                  <Stack spacing={"0"}>
+                    <Heading fontFamily={"Poppins"}>{item.name}</Heading>
+                    <Text color={"gray"}>{item.status}</Text>
+                  </Stack>
+                </EstatePlanItem>
+              ))}
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <Flex
+              flexWrap="wrap"
+              justifyContent={"space-between"}
+              gap="2rem"
+              mt={"5rem"}
+            >
+              {beneficiaries.map((ben, i) => (
+                <EstatePlanItem key={i}>
+                  <BsPersonCircle fontSize={"4rem"} color="darkgreen" />
+
+                  <Stack spacing={"0"}>
+                    <Heading fontFamily={"Poppins"}>{ben.name}</Heading>
+                    <Text color={"gray"}>{ben.relationship}</Text>
+                  </Stack>
+                </EstatePlanItem>
+              ))}
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <Heading >Recommended</Heading>
+            <Flex
+              flexWrap="wrap"
+              justifyContent={"space-between"}
+              gap="2rem"
+              mt={"3rem"}
+            >
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
+                <SimpleWillCard key={i} />
+              ))}
+            </Flex>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </section>
+  );
 }
-EstatePlans.getLayout = function getLayout(page){
-    return(
-        <DashBoardContainer>
-            <MainHeader />
-            <SideNav />
-            {page}
-        </DashBoardContainer>
-    )
-}
+EstatePlans.getLayout = function getLayout(page) {
+  return (
+    <DashBoardContainer>
+      <MainHeader />
+      <SideNav />
+      {page}
+    </DashBoardContainer>
+  );
+};
